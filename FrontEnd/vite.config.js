c1e8ffed29b9f3b02d7f61f77037bc16474400a8
franciscoDastres <franciscomeza.1234@gmail.com>
2025-05-22 19:45:12 -0400

    feat: :zap: fixing deployment settings 2

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -4,4 +4,22 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          // Agrupa otras dependencias grandes aquí si es necesario
+        }
+      }
+    }
+  },
+  server: {
+    port: 5173,
+    open: true
+  }
 })
