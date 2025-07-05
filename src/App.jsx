d740c5ec6037d3a9132870bbbb3d4b5ceb65e1c4
franciscoDastres <franciscomeza.1234@gmail.com>
2025-07-05 17:23:06 -0400

    fix: :art: improving routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { Routes, Route } from "react-router-dom"
 import Layout from "./components/Layout/Layout"
-import HomePage from "./pages/Home"
+import Home from "./pages/Home"
 import NotFound from "./components/NotFound/NotFound"
 import "./index.css"
 function App() {
   return (
     <div className="min-h-screen">
       <Routes>
-        <Route path="/" element={<Layout><HomePage /></Layout>} />
+        <Route path="/" element={<Layout><Home/></Layout>} />
         {/* Futuras rutas */}
         <Route path="*" element={<Layout><NotFound /></Layout>} />
       </Routes>
