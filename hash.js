7ca5e3359c30419e9e6e8bf5af8603a7591e532f
franciscoDastres <franciscomeza.1234@gmail.com>
2025-09-25 16:27:23 -0300

    fix: :bug: deleting has.js

--- hash.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require('bcrypt');
-
-const password = 'tu_contraseña_segura'; // Cambia esto por la contraseña que quieras para el admin
-
-bcrypt.hash(password, 10, (err, hash) => {
-  if (err) throw err;
-  console.log('Hash generado:', hash);
-});
